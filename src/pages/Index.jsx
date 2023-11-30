6831c838327b69823728aa294cb19302d172a2f0
zenovio <zenoviondong@outlook.es>
2023-12-01 03:49:19 +0800

    Iniciando CRM - Clientes

---
+++ src/pages/Index.jsx
@@ -0,0 +1,57 @@
+import { useLoaderData } from "react-router-dom";
+
+export function loader() {
+  const clientes = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        telefono: 102013313,
+        email: "juan@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 2,
+        nombre: 'Karen',
+        telefono: 138198313,
+        email: "karen@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 3,
+        nombre: 'Josue',
+        telefono: 31983913,
+        email: "josue@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 4,
+        nombre: 'Miguel',
+        telefono: 319381983,
+        email: "miguel@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 5,
+        nombre: 'Pedro',
+        telefono: 1398198938,
+        email: "pedro@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+  ];
+  return clientes
+}
+
+function index() {
+
+  const datos = useLoaderData()
+  console.log(datos)
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus {''} <span className="text-blue-700 font-bold">Clientes</span></p>
+    </>
+  )
+}
+
+export default index
